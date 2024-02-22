429e95ad551bff0fa01698de5388a8de5877f68c
klhansen4 <46847874+klhansen4@users.noreply.github.com>
2024-02-22 09:08:18 -0500

    Update TBAInterface.js
    Added Karen's authkey

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "yetZW5TgAYKINAXOd7lgQ4ZiB7ztl7wkzjPIfCChuORa3p2TLF1pM8I0TOqeGRt4";
 /**
  * Get list of teams in event
  *
